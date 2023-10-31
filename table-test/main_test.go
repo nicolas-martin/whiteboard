57f3d213ef8b336aecc3965ce0f75499c8e8ca4f
nicolas-martin <nicolas.martin@coinbase.com>
2023-10-31 15:13:23 -0400

    subtest

---
+++ table-test/main_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSplitSub(t *testing.T) {
+	tests := map[string]struct {
+		input string
+		sep   string
+		want  []string
+	}{
+		"simple":       {input: "a/b/c", sep: "/", want: []string{"a", "b", "c"}},
+		"wrong sep":    {input: "a/b/c", sep: ",", want: []string{"a/b/c"}},
+		"no sep":       {input: "abc", sep: "/", want: []string{"abc"}},
+		"trailing sep": {input: "a/b/c/", sep: "/", want: []string{"a", "b", "c"}},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := Split(tc.input, tc.sep)
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Fatalf("expected: %v, got: %v", tc.want, got)
+			}
+		})
+	}
+}
+
+func TestSplitTable(t *testing.T) {
+	tests := map[string]struct {
+		input string
+		sep   string
+		want  []string
+	}{
+		"simple":       {input: "a/b/c", sep: "/", want: []string{"a", "b", "c"}},
+		"wrong sep":    {input: "a/b/c", sep: ",", want: []string{"a/b/c"}},
+		"no sep":       {input: "abc", sep: "/", want: []string{"abc"}},
+		"trailing sep": {input: "a/b/c/", sep: "/", want: []string{"a", "b", "c"}},
+	}
+
+	for name, tc := range tests {
+		got := Split(tc.input, tc.sep)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Fatalf("%s: expected: %v, got: %v", name, tc.want, got)
+		}
+	}
+}
